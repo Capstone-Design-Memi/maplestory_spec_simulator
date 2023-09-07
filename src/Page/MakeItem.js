786ea0d9dc7b741ef40ef537ccf11a94c5ace3b6
WePung <akgre5@naver.com>
2023-09-07 12:38:20 +0900

    Test : Item
    아이템 테스트

--- src/Page/MakeItem.js
+++ src/Page/MakeItem.js
@@ -1,40 +1,9 @@
 import { Button, Cascader } from "antd";
 import React from "react";
 import { useNavigate } from "react-router-dom";
-const options = [
-  {
-    value: "zhejiang",
-    label: "Zhejiang",
-    children: [
-      {
-        value: "hangzhou",
-        label: "Hangzhou",
-        children: [
-          {
-            value: "xihu",
-            label: "West Lake",
-          },
-        ],
-      },
-    ],
-  },
-  {
-    value: "jiangsu",
-    label: "Jiangsu",
-    children: [
-      {
-        value: "nanjing",
-        label: "Nanjing",
-        children: [
-          {
-            value: "zhonghuamen",
-            label: "Zhong Hua Men",
-          },
-        ],
-      },
-    ],
-  },
-];
+import ItemStat from "../Component/ItemDetail/ItemStat";
+import { DefaultItems } from "../Util/ItemDatas";
+
 const onChange = (value) => {
   console.log(value);
 };
@@ -45,7 +14,7 @@ const MakeItem = () => {
     <div>
       <h2>아이템 제작</h2>
       <Cascader
-        options={options}
+        options={DefaultItems}
         onChange={onChange}
         placeholder="Please select"
       />
@@ -56,6 +25,7 @@ const MakeItem = () => {
       >
         제작
       </Button>
+      <ItemStat />
     </div>
   );
 };
