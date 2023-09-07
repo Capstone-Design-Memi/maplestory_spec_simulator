44e5a373005293bfde6c9afb825a39ab7b3c8f4b
WePung <akgre5@naver.com>
2023-09-07 11:04:00 +0900

    Feat : ClassData
    직업 기본 데이터

--- src/Page/CreateCharacter.js
+++ src/Page/CreateCharacter.js
@@ -1,40 +1,8 @@
 import { Button, Cascader, Input } from "antd";
 import React from "react";
 import { useNavigate } from "react-router-dom";
-const options = [
-  {
-    value: "zhejiang",
-    label: "Zhejiang",
-    children: [
-      {
-        value: "hangzhou",
-        label: "Hangzhou",
-        children: [
-          {
-            value: "xihu",
-            label: "West Lake",
-          },
-        ],
-      },
-    ],
-  },
-  {
-    value: "jiangsu",
-    label: "Jiangsu",
-    children: [
-      {
-        value: "nanjing",
-        label: "Nanjing",
-        children: [
-          {
-            value: "zhonghuamen",
-            label: "Zhong Hua Men",
-          },
-        ],
-      },
-    ],
-  },
-];
+import { Class } from "../Util/Class";
+
 const onChange = (value) => {
   console.log(value);
 };
@@ -46,7 +14,7 @@ const CreateCharacter = () => {
       <h2>캐릭터 생성</h2>
       직업
       <Cascader
-        options={options}
+        options={Class}
         onChange={onChange}
         placeholder="Please select"
       />
---
+++ src/Util/CharatersData.js
@@ -0,0 +1,34 @@
+export const CharactersInfo = [
+    {
+    id:1,
+    name:"test",
+    class:"궁수",
+    job:"패스파인더",
+    level:1,
+    statAttack:1,
+    hp:1,
+    mp:1,
+    stat : {
+        int : 1,
+        str : 1,
+        dex : 1,
+        luk : 1 
+    }
+},
+{
+    id:2,
+    name:"test2",
+    class:"마법사",
+    job:"에반",
+    level:2,
+    statAttack:2,
+    hp:2,
+    mp:2,
+    stat : {
+        int : 2,
+        str : 2,
+        dex : 2,
+        luk : 2 
+    }
+},
+]
