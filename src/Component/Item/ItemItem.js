2cb9c7e02446b306b04fc43f7689f6e9be45ffa8
WePung <akgre5@naver.com>
2023-09-13 23:32:23 +0900

    Edit : ItemItem.js : CSS
    인벤토리 디자인 수정

--- src/Component/Item/ItemItem.js
+++ src/Component/Item/ItemItem.js
@@ -1,29 +1,56 @@
-import { Avatar, Dropdown, Space } from "antd";
-import React from "react";
-import ItemStat from "../ItemDetail/ItemStat";
-
-const items = [
-  {
-    key: "1",
-    label: <ItemStat />,
-  },
-];
+import React, { useEffect, useState } from "react";
+import { ChractersInfo } from "../../Util/CharatersData";
+import { DefaultItems } from "../../Util/ItemDatas";
+import { InventoryWrap, ItemBoxWrap } from "../../Style/Item";
 
 const ItemItem = () => {
+  const myItems = ChractersInfo[0].equipments;
+  const [col, setCol] = useState(0);
+  const inventoryItem = DefaultItems;
+
+  const testInventoryItem = inventoryItem.map((it) => {
+    return (
+      <InventoryWrap>
+        <img src={it.imageUrl} />
+      </InventoryWrap>
+    );
+  });
+
+  const myItemMap = myItems.map((item) => {
+    const myItemsCategory = item.category;
+    const inventoryItemMap = inventoryItem.map((it) => {
+      if (item.category == it.category) {
+        return item;
+      }
+    });
+    return inventoryItemMap;
+  });
+
+  console.log(myItemMap);
+
   return (
     <div>
-      아이템
-      <Dropdown
-        menu={{
-          items,
+      <h3>장착비교</h3>
+      <div></div>
+      <h3>인벤</h3>
+      <div
+        style={{
+          width: `${testInventoryItem.length >= 10 ? 475 : 51}px`,
+          height: `${
+            testInventoryItem.length >= 10
+              ? ((testInventoryItem.length - (testInventoryItem.length % 10)) /
+                  10 +
+                  1) *
+                51
+              : 51
+          }px`,
+          border: "1px solid #C4C4C4",
+          marginLeft: "5px",
+          borderRadius: "5px",
         }}
       >
-        <a onClick={(e) => e.preventDefault()}>
-          <Space>
-            <Avatar />
-          </Space>
-        </a>
-      </Dropdown>
+        <ItemBoxWrap>{testInventoryItem}</ItemBoxWrap>
+      </div>
     </div>
   );
 };
