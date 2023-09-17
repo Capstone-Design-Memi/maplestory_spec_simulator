f21fe66084f122b8461fe32ef8caad0e3b87d962
WePung <akgre5@naver.com>
2023-09-17 15:06:20 +0900

    Feat : MakeItem
    아이템 추가시 메인 화면에 추가한 아이템 표시

--- src/Component/Item/ItemItem.js
+++ src/Component/Item/ItemItem.js
@@ -7,9 +7,24 @@ import { useCookies } from "react-cookie";
 const ItemItem = () => {
   const [cookies, setCookie, removeCookie] = useCookies();
   const myItems = ChractersInfo[0].equipments;
-  const [col, setCol] = useState(0);
+  const [hover, setHover] = useState(false);
+  const [testLocalItems, setTestLocalItems] = useState([]);
   const inventoryItem = DefaultItems;
 
+  // const test = JSON.parse(localStorage);
+  const allKeys = Object.keys(localStorage);
+  const allKeyMap = allKeys.map((item) => {
+    return JSON.parse(localStorage.getItem(item));
+  });
+  console.log(allKeyMap);
+
+  const v = allKeyMap.map((item) => {
+    return (
+      <InventoryWrap>
+        <img src={item[0].imageUrl} />
+      </InventoryWrap>
+    );
+  });
   const testInventoryItem = inventoryItem.map((it) => {
     return (
       <InventoryWrap>
@@ -19,33 +34,46 @@ const ItemItem = () => {
   });
 
   const myItemMap = myItems.map((item) => {
-    const myItemsCategory = item.category;
-    const inventoryItemMap = inventoryItem.map((it) => {
+    const myItemCategory = item.category;
+    const inventoryItemCategoryMap = inventoryItem.map((it) => {
+      const inventoryItemCategory = it.category;
       if (item.category == it.category) {
-        return item;
+        // console.log(`item : ${item.category}`);
+        // console.log(`it : ${it.category}`);
+        return item.category;
       }
     });
-    return inventoryItemMap;
   });
-
-  console.log(myItemMap);
-
+  // console.log(myItemMap);
+  // 데이터가 JSON형태로 들어가 있어 가져올떄는 파싱을 통해 객체로 가져옴
+  // const testLocal = JSON.parse(localStorage.getItem("test"));
+  // console.log(testLocal);
   return (
     <div>
+      <button
+        onClick={() => {
+          // 객체를 localstorage에 담을 때 object로 저장이 되어 객체를 JSON형태로 파싱
+          localStorage.setItem("test", JSON.stringify({ inventoryItem }));
+        }}
+      >
+        로컬 생성
+      </button>
+      <button
+        onClick={() => {
+          localStorage.removeItem("test");
+        }}
+      >
+        로컬 삭제
+      </button>
       <h3>장착비교</h3>
       <div></div>
       <h3>인벤</h3>
       <div
         style={{
-          width: `${
-            testInventoryItem.length >= 10 ? 475 : 51 * testInventoryItem.length
-          }px`,
+          width: `${allKeys.length >= 10 ? 475 : 51 * allKeys.length}px`,
           height: `${
-            testInventoryItem.length >= 10
-              ? ((testInventoryItem.length - (testInventoryItem.length % 10)) /
-                  10 +
-                  1) *
-                51
+            allKeys.length >= 10
+              ? ((allKeys.length - (allKeys.length % 10)) / 10 + 1) * 51
               : 51
           }px`,
           border: "1px solid #C4C4C4",
@@ -53,7 +81,7 @@ const ItemItem = () => {
           borderRadius: "5px",
         }}
       >
-        <ItemBoxWrap>{testInventoryItem}</ItemBoxWrap>
+        <ItemBoxWrap>{v}</ItemBoxWrap>
       </div>
     </div>
   );
--- src/Page/MakeItem.js
+++ src/Page/MakeItem.js
@@ -1,30 +1,40 @@
-import { Button, Cascader } from "antd";
-import React from "react";
+import { Button, Cascader, Form, Select } from "antd";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import ItemStat from "../Component/ItemDetail/ItemStat";
 import { DefaultItems } from "../Util/ItemDatas";
 
-const onChange = (value) => {
-  console.log(value);
-};
-
 const MakeItem = () => {
+  const [itemId, setItemId] = useState(0);
   const navigator = useNavigate();
+
+  const testLocal = JSON.parse(localStorage.getItem("test"));
+
+  const onFinish = (value) => {
+    const defaultItemMap = DefaultItems.map((item) => {
+      if (item.name == value.makeItem) {
+        const testItemId = { itemId: itemId };
+        const testInput = [item].concat([testItemId]);
+        console.log(testInput);
+        localStorage.setItem(`test${itemId}`, JSON.stringify(testInput));
+        setItemId(itemId + 1);
+      }
+    });
+  };
   return (
     <div>
       <h2>아이템 제작</h2>
-      <Cascader
-        options={DefaultItems}
-        onChange={onChange}
-        placeholder="Please select"
-      />
-      <Button
-        onClick={() => {
-          navigator("/main");
-        }}
-      >
-        제작
-      </Button>
+      <Form onFinish={onFinish}>
+        <Form.Item label="makeItem" name="makeItem">
+          <Cascader options={DefaultItems} />
+        </Form.Item>
+
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            제작
+          </Button>
+        </Form.Item>
+      </Form>
     </div>
   );
 };
