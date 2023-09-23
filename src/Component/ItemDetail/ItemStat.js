15f8520c8a6745c7a2a1df5cfef62f59fcd0fdc1
WePung <akgre5@naver.com>
2023-09-24 00:52:55 +0900

    Fix : bug
    ItemStat 이미지 버그 수정

--- src/Component/ItemDetail/ItemStat.js
+++ src/Component/ItemDetail/ItemStat.js
@@ -1,10 +1,10 @@
 import React, { useEffect } from "react";
 //import { DefaultItems } from "../../Util/ItemDatas";
-import styled from 'styled-components';
-import Cover_img from '../../images/frame/cover.png';
-import Top_frame_img from '../../images/frame/top.png';
-import Bottom_frame_img from '../../images/frame/bottom.png';
-import Line_img from '../../images/frame/line.png';
+import styled from "styled-components";
+import Cover_img from "../../images/frame/cover.png";
+import Top_frame_img from "../../images/frame/top.png";
+import Bottom_frame_img from "../../images/frame/bottom.png";
+import Line_img from "../../images/frame/line.png";
 import StarLine from "./StarLine";
 import ItemName from "./ItemName";
 import EquipDisable from "./EquipDisable";
@@ -20,77 +20,84 @@ const Wrapper = styled.div`
   padding: 0em;
   margin: 0em;
   width: 261px;
-  background-image: url('${Cover_img}');
+  background-image: url("${Cover_img}");
   background-position: left top;
-`
+`;
 
 export const TopFrame = styled.div`
   height: 13px;
-  background-image: url('${Top_frame_img}');
-`
+  background-image: url("${Top_frame_img}");
+`;
 
 export const BottomFrame = styled.div`
   height: 13px;
-  background-image: url('${Bottom_frame_img}');
-`
+  background-image: url("${Bottom_frame_img}");
+`;
 
 export const Line = styled.div`
   height: 1px;
-  background-image: url('${Line_img}');
-`
+  background-image: url("${Line_img}");
+`;
 
 export const Line3 = styled.div`
   height: 3px;
-  background-image: url('${Line_img}');
-`
+  background-image: url("${Line_img}");
+`;
 
 export const Line7 = styled.div`
   height: 7px;
-  background-image: url('${Line_img}');
-`
+  background-image: url("${Line_img}");
+`;
 
 export const DotLine = styled.div`
   height: 2px;
-  background-image: url('${DotLine_img}');
-`
+  background-image: url("${DotLine_img}");
+`;
 
-const ItemStat = ({item,extraInfo}) => {
+const ItemStat = ({ item, extraInfo }) => {
   return (
     <Wrapper>
-      <TopFrame/>
-      <StarLine currentStar={item.star} maxStar={extraInfo.maxStar}/>
-      <Line3/>
-      <ItemName name={item.name}/>
-      <ItemGrade grade={item.grade}/>
-      <EquipDisable/>
-      <Line7/>
-      <DotLine/>
-      <Line7/>
-      <Line3/>
-      <ItemImagePart grade={item.grade}
-                     imgUrl={item.imageUrl}
-                     stateChageAmount={extraInfo.stateChageAmount}
-                     reqLev={extraInfo.reqLev}/>
-      <ZeroPersent/>
-      <JobLine/>
-      <Line7/>
-      <DotLine/>
-      <Line3/>
-      <BaseAndFlame category={item.category} base={item.base} flame={item.flame} scroll={item.scroll}/>
-      <Line7/>
-      <DotLine/>
-      <PotentialLine grade={item.grade} potential={item.potential}/>
-      <Line7/>
-      <BottomFrame/>
+      <TopFrame />
+      <StarLine currentStar={item.star} maxStar={extraInfo?.maxStar} />
+      <Line3 />
+      <ItemName name={item.name} />
+      <ItemGrade grade={item.grade} />
+      <EquipDisable />
+      <Line7 />
+      <DotLine />
+      <Line7 />
+      <Line3 />
+      <ItemImagePart
+        grade={item.grade}
+        imgUrl={item.imageUrl}
+        stateChageAmount={extraInfo?.stateChageAmount}
+        reqLev={extraInfo?.reqLev}
+      />
+      <ZeroPersent />
+      <JobLine />
+      <Line7 />
+      <DotLine />
+      <Line3 />
+      <BaseAndFlame
+        category={item.category}
+        base={item.base}
+        flame={item.flame}
+        scroll={item.scroll}
+      />
+      <Line7 />
+      <DotLine />
+      <PotentialLine grade={item.grade} potential={item?.potential} />
+      <Line7 />
+      <BottomFrame />
     </Wrapper>
   );
 };
 
 const otherInformation = {
-  maxStar : 20,
-  stateChageAmount : 2010914,
-  reqLev : 150
-}
+  maxStar: 20,
+  stateChageAmount: 2010914,
+  reqLev: 150,
+};
 
 // const itemEx = {
 //   "name": "데아 시두스 이어링",
@@ -185,5 +192,5 @@ const itemEx2 = {
     mp: 2250,
   },
   scissors: 10,
-}
+};
 export default ItemStat;
