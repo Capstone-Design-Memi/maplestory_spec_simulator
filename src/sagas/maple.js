195fef9e1dda10149938363bdcfc8b85fca9665d
WePung <akgre5@naver.com>
2023-09-25 21:14:58 +0900

    Add : proxy
    cors오류 수정(수정)

--- src/sagas/maple.js
+++ src/sagas/maple.js
@@ -7,19 +7,21 @@ import {
   LOAD_MAPLE_CHRACTER_SUCCESS,
   LOAD_MAPLE_CHRACTER_REQUEST,
 } from "../constants/actionTypes";
+import { act } from "react-dom/test-utils";
 
 function loadCharacterAPI(data) {
   //단일 게시글 로딩
-  return axios.get(`characters?name=${data}&scopes=pet,equip,cash,symbol`);
+  return axios.get(`characters?name=${data}&scopes=pet,equip,cash,symbol`, {
+    headers: {},
+  });
 }
 
 function* loadCharacter(action) {
   try {
     const result = yield call(loadCharacterAPI, action.data);
-    console.log(result);
     yield put({
       type: LOAD_MAPLE_CHRACTER_SUCCESS,
-      data: result,
+      data: result.data,
     });
   } catch (err) {
     console.error(err);
