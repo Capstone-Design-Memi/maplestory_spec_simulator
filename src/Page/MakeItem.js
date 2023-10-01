c521e7c0c8b87045eeaffc8305d6b1461f9193db
WePung <akgre5@naver.com>
2023-10-02 02:16:24 +0900

    Fix : MakeItem.js
    Nan오류 수정

--- src/Page/MakeItem.js
+++ src/Page/MakeItem.js
@@ -4,34 +4,47 @@ import { useNavigate } from "react-router-dom";
 import ItemStat from "../Component/ItemDetail/ItemStat";
 import { DefaultItems } from "../Util/ItemDatas";
 import Enforce from "../Component/Enforce/Enforce";
+import { Cookies, useCookies } from "react-cookie";
 
 const MakeItem = () => {
+  const [cookies, setcookie, removecookie] = useCookies();
   const [itemId, setItemId] = useState(0);
   const navigator = useNavigate();
   const testLocal = JSON.parse(localStorage.getItem("test"));
+
   const allKeys = Object.keys(localStorage);
   const allKeyMap = allKeys.map((item) => {
     return JSON.parse(localStorage.getItem(item));
   });
+
+  //
+  const localstorageMap = Object.keys(localStorage).map((key) => {
+    const outPut = localStorage[key];
+    return JSON.parse(outPut);
+  });
   const testId = allKeys[allKeys.length - 1];
+
   useEffect(() => {
     if (allKeys.length !== 0) {
-      const testReplace = testId?.replace("test", "");
+      const testReplace = testId?.replace("InventoryItem", "");
+      console.log(testReplace);
       setItemId(parseInt(testReplace) + 1);
-      console.log(itemId);
     } else {
       setItemId(0);
     }
   }, []);
-  console.log(itemId);
 
   const onFinish = (value) => {
+    // 현재 선택된 캐릭터 아이디 판별 필요
     const defaultItemMap = DefaultItems.map((item) => {
       if (item.name == value.makeItem) {
         const testItemId = { itemId: itemId };
         const testInput = [item].concat([testItemId]);
         console.log(testInput);
-        localStorage.setItem(`test${itemId}`, JSON.stringify(testInput));
+        localStorage.setItem(
+          `InventoryItem${itemId}`,
+          JSON.stringify(testInput)
+        );
         setItemId(itemId + 1);
       }
     });
@@ -41,6 +54,7 @@ const MakeItem = () => {
     const optionItem = { value: item.name, label: item.name };
     return optionItem;
   });
+
   return (
     <div>
       <h2>아이템 제작</h2>
