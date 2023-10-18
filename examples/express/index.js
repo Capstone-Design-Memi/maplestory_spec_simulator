84680cea04f7472807b9b313485f9f2fa16c62aa
WePung <akgre5@naver.com>
2023-10-18 18:03:07 +0900

    Update : node version
    node 업데이트로 인한 변경사항

--- examples/express/index.js
+++ examples/express/index.js
@@ -1,19 +1,19 @@
-const express = require('express');
-const { MapleUtilsParser } = require('mapleutils-parser-js');
+const express = require("express");
+const { MapleUtilsParser } = require("mapleutils-parser-js");
 const app = express();
-const port = 3030;
+const port = 3000;
 
-app.get('/character/:name', async (req, res) => {
-    const character = await MapleUtilsParser.new().getCharacter({
-        name: req.params.name,
-        cash: false,
-        pet: false,
-        equip: true,
-        symbol: true,
-    });
-    res.send(JSON.stringify(character, undefined, 4));
+app.get("/character/:name", async (req, res) => {
+  const character = await MapleUtilsParser.new().getCharacter({
+    name: req.params.name,
+    cash: false,
+    pet: false,
+    equip: true,
+    symbol: true,
+  });
+  res.send(JSON.stringify(character, undefined, 4));
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
