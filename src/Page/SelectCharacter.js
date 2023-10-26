d50f08c79362aba9a9f350822f528d3c94fa502f
WePung <akgre5@naver.com>
2023-10-26 11:56:51 +0900

    Edit : SelectCharacterPage : CSS
    CSS 수정(틀만)

--- src/Page/SelectCharacter.js
+++ src/Page/SelectCharacter.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import CharaterList from "../Component/SelectCharacter/CharaterList";
-import { Button, Radio } from "antd";
+import { Button, Col, Radio, Row } from "antd";
 import CreateCharacter from "./CreateCharacter";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
@@ -12,25 +12,43 @@ const SelectCharacter = () => {
   const navigator = useNavigate();
 
   return (
-    <div>
-      <Button
-        onClick={() => {
-          navigator("/createcharacter");
-        }}
-      >
-        캐릭터 생성
-      </Button>
-      <Radio.Group
-        value={toggle}
-        onChange={(e) => {
-          setToggle(e.target.value);
-        }}
-      >
-        <Radio value={false}>선택</Radio>
-        <Radio value={true}>삭제</Radio>
-      </Radio.Group>
-      <h2>캐릭터 선택</h2>
-      <CharaterList toggle={toggle} />
+    <div
+      style={{
+        backgroundImage:
+          "url(../../assets/maplestory_character_select_menu_by_pixel_rydol_de9aaup-fullview.jpg)",
+        height: "120vh",
+        backgroundRepeat: " no-repeat",
+        backgroundSize: "cover",
+      }}
+    >
+      <Row style={{ transform: "translateY(25rem)", height: "34rem" }}>
+        <Col xs={18} md={18}>
+          <CharaterList toggle={toggle} />
+        </Col>
+        <Col xs={6} md={6}>
+          <div>캐릭터 스텟 및 선택버튼</div>
+        </Col>
+      </Row>
+      <Row style={{ transform: "translateY(25rem)", height: "13.3rem" }}>
+        <Col xs={24} md={24}>
+          <Button
+            onClick={() => {
+              navigator("/createcharacter");
+            }}
+          >
+            캐릭터 생성
+          </Button>
+          <Radio.Group
+            value={toggle}
+            onChange={(e) => {
+              setToggle(e.target.value);
+            }}
+          >
+            <Radio value={false}>선택</Radio>
+            <Radio value={true}>삭제</Radio>
+          </Radio.Group>
+        </Col>
+      </Row>
     </div>
   );
 };
