bd8e18d9fc2e86f8dda9eb52600cd69d1305b26d
WePung <akgre5@naver.com>
2023-11-02 11:58:58 +0900

    Feat : AutoNavigate
    쿠키에 cId값이 없을 경우 자동으로 캐릭터 선택창으로 이동 기능 추가

--- src/Page/Main.js
+++ src/Page/Main.js
@@ -1,11 +1,20 @@
 import { Col, Row } from "antd";
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import ItemList from "../Component/Item/ItemList";
 import StatWindow from "../Component/Stat/StatWindow";
 import EquipmentInventory from "../Component/EquipmentInventory/EquipmentInventory";
 import { AppContext } from "../App";
+import { useNavigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
 
 const Main = () => {
+  const [cookies] = useCookies();
+  const navigator = useNavigate();
+  useEffect(() => {
+    if (cookies.cId === undefined) {
+      navigator("/");
+    }
+  }, []);
   return (
     <div>
       <h2>메인</h2>
