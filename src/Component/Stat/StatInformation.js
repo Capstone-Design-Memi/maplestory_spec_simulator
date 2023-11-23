e9cf89c83da3cf8f6c1e1552e27fa8d309bb61fb
mareto1017 <oktobroesperi@naver.com>
2023-11-23 11:03:23 +0900

    Edit : StatInformation.js

--- src/Component/Stat/StatInformation.js
+++ src/Component/Stat/StatInformation.js
@@ -6,12 +6,17 @@ import styled from "styled-components";
 
 import { AttackBack, CharacterInfo, LayerName, MainStatBack, Stat, StatBackgrnd, ApDistributeButton, 
     UtilityBack, AbilityButton, HyperButton, MainStatFont, AttactFont, UtilityFont, DefenseFont, LevelText,
-     CharacterImg, NameText, GuildText } from "./Styles";
+     CharacterImg, NameText, GuildText, HyperBack } from "./Styles";
 
 const StatInformation = () => {
     const [cookies, setCookie, removeCookie] = useCookies();
     const [toggle, setToggle] = useState(false);
+    const [hyperToggle, setHyperToggle] = useState(false);
     const [information, setInformation] = useState(JSON.parse(localStorage.getItem(`testChItem${cookies.cId.cId}`))[0]);
+
+    const showHyper = () => {
+        setHyperToggle(!hyperToggle);
+    }
 
     const test = (e) => {
         setToggle(!toggle)
@@ -34,6 +39,9 @@ const StatInformation = () => {
             </CharacterInfo>
             <StatBackgrnd>
                 <Stat/>
+                {
+                    hyperToggle && <HyperBack></HyperBack>
+                }
                 <AttackBack>
                     <div style={{width:"448px", height:"35px", boxSizing:"border-box"}}></div>
                     <div style={{overflow:"hidden", marginTop:"2px"}}>
@@ -55,7 +63,7 @@ const StatInformation = () => {
                     }
                 </UtilityBack>
                 <div style={{overflow:"hidden"}}>
-                    <HyperButton/>
+                    <HyperButton onClick={showHyper}/>
                     <AbilityButton/>
                 </div>
             </StatBackgrnd>
