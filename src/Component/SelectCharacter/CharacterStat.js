7b40efbbf0c0f41010c4b8ffdaa9086c7b793c20
WePung <akgre5@naver.com>
2023-11-23 10:37:17 +0900

    Fix : CharacterRemove
    캐릭터 삭제하면 cId값을 확인 못 하던 버그 수정

--- src/Component/SelectCharacter/CharacterStat.js
+++ src/Component/SelectCharacter/CharacterStat.js
@@ -34,9 +34,9 @@ const CharacterStat = () => {
   const chInfoMapFilterUndefined = chInfoMap.filter(
     (item) => item.cookieInputData.id != undefined
   );
-  const chInfo = chInfoMapFilterUndefined[0].cookieInputData.data[0];
-  console.log(cookies.cId);
-  console.log(chInfo);
+  const chInfo = cookies.cId
+    ? chInfoMapFilterUndefined[0].cookieInputData.data[0]
+    : "캐릭터를 선택해주세요.";
 
   const mainStat = jobClass.map((item) => {
     const type = item.children;
